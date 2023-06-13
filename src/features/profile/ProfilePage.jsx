07aff1555cc46e3f21483e62344ad7f64d7a64a7
Souljeee <drobitek76106055@gmail.com>
2023-06-13 19:27:35 +0300

    feat(profile): add profile page

---
+++ src/features/profile/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../home/components/Navbar/Navbar"
+
+const ProfilePage = () => {
+    return <div>
+        <Navbar />
+    </div>    
+} 
+
+export default ProfilePage
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "../features/home/Home"
 import LikesPage from "../features/likes/LikesPage"
+import ProfilePage from "../features/profile/ProfilePage"
 
 const Router = () => {
     return <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/likes' element={<LikesPage />} />
+            <Route path='/profile' element={<ProfilePage />} />
             <Route element={<div>Page Not Found</div>} path='*'/>
         </Routes>
     </BrowserRouter>
