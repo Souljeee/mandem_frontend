8c6c64b78fcf8105ded4a6fc6dda1a04ebb50b6d
Souljeee <drobitek76106055@gmail.com>
2023-06-17 07:37:02 +0300

    feat(profile): add final profile page version

--- src/features/profile/components/ProfilePageContent.jsx
+++ src/features/profile/components/ProfilePageContent.jsx
@@ -1,4 +1,6 @@
 import styles from "./ProfilePageContent.module.css"
+import { interests } from "../../home/components/interest_chip/chips.data"
+import InterestChip from "../../home/components/interest_chip/InterestChip"
 
 const ProfilePageContent = () => {
     return <div className={styles.contentContainer}>
@@ -15,6 +17,20 @@ const ProfilePageContent = () => {
             <div className={styles.phoneNumber}>Телефон: +79267568745</div>
             <div className={styles.interestsContainer}>
                 <div className={styles.interestsTitle}>Интересы:</div>
+                <div className={styles.chipContainer}>
+                {interests.map(interest => (
+                    <div className={styles.chip}>
+                        <InterestChip key={interest.id} interest={interest} />
+                    </div>
+                ))}
+                </div>
+            </div>
+            <div className={styles.aboutInfo}>
+                <div className={styles.aboutTitle}>О себе:</div>
+                <div className={styles.aboutContent}>Я элегантная Мелисса и чувственная девушка, могу предложить приятное
+                    времяпрепровождение для тебя, как твоя девушка. Вы не пожалеете о моей компании
+                    , я полна очарования и готова поделиться всем, что заставит вас чувствовать себя прекрасно.
+                    Не ждите, просто позвоните мне сейчас! Не пропустите ожидающее вас удовольствие!</div>
             </div>
         </div>
     </div>
